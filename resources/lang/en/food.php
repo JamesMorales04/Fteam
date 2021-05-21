6a425637b7fdcb0e92575126798fb469ce13a267
Nicolas Raigosa <nraigosam@eafit.edu.co>
2021-05-21 18:05:41 -0500

    separasion de las vista de menu en usuario y admin

---
+++ resources/lang/en/food.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    //Food
+    'foods' => 'Show all food',
+    'askForIngredients' => 'Only ingredients',
+    'backFood' => 'Back to food',
+    'foodName' => 'Food Name',
+    'foodId' => 'Food Id',
+    'description' => 'Description',
+    'status' => 'Status',
+    'available' => 'Available',
+    'notAvailable' => 'Not available',
+    'menu' => 'Menu',
+    'recipe' => 'Recipe',
+    'topThree' => 'Top three food',
+    'best-selling' => 'The three best-selling products are',
+    'createFood' => 'Create food',
+    'updateFood' => 'Uddate food',
+];
---
+++ resources/lang/es/food.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    //Food
+    'foods' => 'Mostrar todas las comidas',
+    'askForIngredients' => 'Pedir ingredientes',
+    'backFood' => 'Volver a Comida',
+    'foodName' => 'Nombre de la Comida',
+    'foodId' => 'Id de la comida',
+    'description' => 'Descripción',
+    'status' => 'Estado',
+    'available' => 'Disponible',
+    'notAvailable' => 'Agotado',
+    'menu' => 'Menu',
+    'recipe' => 'Receta',
+    'topThree' => 'Top 3 de Comida',
+    'best-selling' => 'Los tres productos más vendidos son',
+    'createFood' => 'Añadir comida',
+    'updateFood' => 'Actializar receta',
+];
