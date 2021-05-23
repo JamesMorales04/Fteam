b0caeab891824d8809ad079e14d3f1fa86aa2b69
Santiago Bedoya <sbedoyac1@eafit.edu.co>
2021-05-23 12:48:06 -0500

    Boton de PayPal

--- resources/views/shopping/cart.blade.php
+++ resources/views/shopping/cart.blade.php
@@ -72,18 +72,12 @@
                                     <a method="GET" href="{{ route('shop.buy') }}">
                                         <button class="btn btn-outline-primary"> {{ __('cart.buy') }} </button>
                                     </a>
-                                    <br>
-                                    <br>
-                                    <div class="flex-center position-ref full-height">
-  
-                                        <div class="content">
-                                              
-                                            <table border="0" cellpadding="10" cellspacing="0" align="center"><tr><td align="center"></td></tr><tr><td align="center"><a href="https://www.paypal.com/in/webapps/mpp/paypal-popup" title="How PayPal Works" onclick="javascript:window.open('https://www.paypal.com/in/webapps/mpp/paypal-popup','WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"><img src="https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-200px.png" border="0" alt="PayPal Logo"></a></td></tr></table>
-                              
-                                            <center><a method="GET" href="{{ route('payment') }}" class="btn btn-outline-primary">{{ __('cart.buyWithPayPal') }}</a></center>
-                              
+                                    <a method="GET" href="{{ route('payment') }}" class="btn btn-outline-primary">
+                                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                
                                         </div>
-                                    </div>
+                                        <img class="img-fluid" src="https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-200px.png" width="90" height="90" alt="" />
+                                    </a>
                                 @else
                                     <button class="btn outline-primary"> {{ __('cart.buy') }} </button>
                                 @endif
